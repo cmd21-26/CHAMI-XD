de64216313b33027cac52b684bca47332ff3aed6
cmd21-26 <sadamalidilrukshi192@gmail.com>
2024-12-18 07:00:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9kw0DYDL#62yqeDrY4vtidGgotn3PtyVbFGOlW5iQiI-sIol537k",
+SESSION_ID: process.env.SESSION_ID || "ssA1GIzb#R9yEsoinotb0rVRU-72cIoSXuQhMg8m5QIY870Sabq4",
 MODE : process.env.MODE || "public",// groups/public/private
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
